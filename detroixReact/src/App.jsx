ea3b659e6c8e76eda17e80ee2d7b5a64082c956c
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-07-08 13:46:01 -0600

    getting use to it

---
+++ detroixReact/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+// import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ detroixReact/src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header() {
+
+    return (
+        <header role="banner">
+            <div className="row">
+                <div className="col-6">
+                    <h6>www.DEMOLINK.ORG |</h6>
+                </div>
+                <div className="col-6 d-flex justify-content-end">
+                    <h6>SUPPORT|FAQS|SITEMAP|HELP</h6>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
